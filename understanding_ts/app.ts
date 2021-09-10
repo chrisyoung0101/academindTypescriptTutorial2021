bbdfe59f3f0f9b7550d835776291b9d17c50e92d
Chris Young <chrisyoung0101@gmail.com>
2021-09-10 16:42:33 -0500

    demonstrate main types

--- understanding_ts/app.ts
+++ understanding_ts/app.ts
@@ -1,1 +1,17 @@
-console.log('Code goes here...');
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+  //because we are attempting to log a string and a number we need to do the math outside the clg so we get our expected result.
+  const result = n1 + n2;
+  if (showResult) {
+    console.log(phrase + result);
+  } else {
+    return result;
+  }
+}
+
+const number1 = 5;
+const number2 = 2.8;
+const printResult = true;
+const resultPhrase = 'Result is: ';
+
+const result = add(number1, number2, printResult, resultPhrase);
+console.log(result);
