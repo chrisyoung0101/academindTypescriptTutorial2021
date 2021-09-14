1ebf996c623595375e32a54246561e951b8d2f00
Chris Young <chrisyoung0101@gmail.com>
2021-09-14 16:28:56 -0500

    Arrays in TS

--- understanding_ts/app.ts
+++ understanding_ts/app.ts
@@ -1,11 +1,16 @@
 // const person: {
-//     // key : type values 
+//     // key : type values
 //     name: string;
 //     age: number;
 // } = {
 const person = {
   name: 'Maximilian',
   age: 30,
+  hobbies: ['Sports', 'Cooking'],
 };
 
-console.log(person.);
+console.log(person);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
